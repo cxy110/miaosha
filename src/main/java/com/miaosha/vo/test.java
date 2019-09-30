86f09bd739e165d2c738843dc3b265cb8d07fd7b
cxy <1449826834@qq.com>
2019-09-30 11:34:47 +0800

    秒杀数据库

--- src/main/java/com/miaosha/vo/test.java
+++ src/main/java/com/miaosha/vo/test.java
@@ -8,18 +8,6 @@ package com.miaosha.vo;
  */
 public class test {
   public static void main(String[] args) {
-    Integer a = new Integer("5");
-    Integer b = new Integer("5");
-    Integer c=5;
-    int d=5;
-    Integer e=128;
-    int f=128;
-    System.out.println(a==b);
-    System.out.println(b==c);
-    System.out.println(b==d);
 
-    System.out.println(c==b);
-    System.out.println(e==f);
-    System.out.println(c==d);
   }
 }
